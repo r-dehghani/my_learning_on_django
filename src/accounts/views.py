bc9dcf9487a479f8987cd887f42642763bad86e7
Dariush Dehghani <r.dehghani.90@gmail.com>
2022-07-31 00:32:56 +0430

    13- add log out feature

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,30 +3,34 @@ from django.contrib.auth import authenticate, login, logout
 
 
 def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {
-                "error": "invalid username or password!!!",
-            }
-            return render(request, "accounts/login.html", context=context)
-        login(request, user)
-        return redirect("/")
-        # context = {
-        #     "username": username,
-        #     "password": password,
-        # }
-    return render(request, 'accounts/login.html', {})
+    if request.user.is_authenticated:
+        return render(request, "accounts/already_loged_in.html", {})
+    else:
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is None:
+                context = {
+                    "error": "invalid username or password!!!",
+                }
+                return render(request, "accounts/login.html", context=context)
+            login(request, user)
+            return redirect("/")
+            # context = {
+            #     "username": username,
+            #     "password": password,
+            # }
+        return render(request, 'accounts/login.html', {})
 
 
 def logout_view(request):
-    logout(request)
-    context = {
+    if request.user.is_authenticated:
+        logout(request)
+        context = {
 
-    }
-    return render(request, 'accounts/logout.html', context=context)
+        }
+        return render(request, 'accounts/logout.html', context=context)
 
 
 def register_view(request):
