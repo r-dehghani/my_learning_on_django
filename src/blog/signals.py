27794faecb7ee14171f3e411001021222ad5db81
Dariush Dehghani <r.dehghani.90@gmail.com>
2022-08-11 18:21:12 +0430

    26- auto generate slug fields with recursion for article model

--- src/blog/signals.py
+++ src/blog/signals.py
@@ -2,32 +2,25 @@ from django.db.models.signals import pre_save, post_save
 from django.utils.text import slugify
 from .models import Article
 # بعد ساختن سیگنال هامون باید اون ها رو دارد فایل apps.py بکنیم
+from .utils import slugify_instance_title
 
 #  ====================================================================
 
 
-def article_slug_pre_save(sender, instance, *args, **kwargs):
-    print("pre save!")
+def article_pre_save(sender, instance, *args, **kwargs):
+    # print('pre_save')
     if instance.slug is None:
-        print("<<<<<<<<<<<<<<<<<<<<<<<None")
-        instance.slug = slugify(instance.title)
+        slugify_instance_title(instance, save=False)
 
 
-pre_save.connect(article_slug_pre_save, sender=Article)
-
-#  ====================================================================
-
-#  ====================================================================
+pre_save.connect(article_pre_save, sender=Article)
 
 
-def article_slug_post_save(sender, instance, created, *args, **kwargs):
-    print("post save!")
+def article_post_save(sender, instance, created, *args, **kwargs):
+    # print('post_save')
     if created:
-        print("<<<<<<<<<<<<<<<<<<<<<<<created")
-        instance.slug = slugify(instance.title)
-        instance.save()
+        slugify_instance_title(instance, save=True)
 
 
-post_save.connect(article_slug_post_save, sender=Article)
-
+post_save.connect(article_post_save, sender=Article)
 #  ====================================================================
