a3a35c7d90fc17fd513768d534f90cff52abcbff
Dariush Dehghani <r.dehghani.90@gmail.com>
2022-08-28 22:03:18 +0430

    33- building shopping app -models

---
+++ src/shopping/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import User, Customer, Order, OrderItem, Product, ShippingAddress
+# Register your models here.
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = []
+
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Product)
+admin.site.register(ShippingAddress)
